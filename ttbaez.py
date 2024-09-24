28b590e7bf50376fca3c068ccdb6e0b3c237793c
Baez <Baezlopezolsen@dusd.org>
2024-09-24 15:27:06 -0700

    Iteration 1 done "Make your first modification. Set the pen up when you create the turtle object, and set the pen down after you move it to its starting position."

--- ttbaez.py
+++ ttbaez.py
@@ -2,30 +2,48 @@
 #   Add code to make turtles move in a circle and change colors.
 import turtle as trtl
 
+
+class muchasTortugas:
+  #init Method
+  def __init__(self, numTurtles, shapes, colors):
+    self.numTurtles = numTurtles
+    self.shapes = shapes
+    self.colors = colors
+    self.turtles = []
+    for each in range(self.numTurtles):
+      t = trtl.Turtle(shape=self.shapes[each])
+      t.color(colors[each])
+      self.turtles.append(t)
+  def draw(self):
+    startx = 0
+    starty = 0
+    for t in self.turtles:
+      t.penup()
+      t.goto(startx, starty)
+      t.pendown()
+      t.right(45)     
+      t.forward(50)
+      startx = startx + 50
+      starty = starty + 50
+    
+
+
+
+
+
+
 # create an empty list of turtles
-my_turtles = []
 
 # use interesting shapes and colors
 turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
 turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 
-for s in turtle_shapes:
-  t = trtl.Turtle(shape=s)
-  my_turtles.append(t)
-
-#  
-startx = 0
-starty = 0
-
-#
-for t in my_turtles:
-  t.goto(startx, starty)
-  t.right(45)     
-  t.forward(50)
+turtles = muchasTortugas(6, turtle_shapes, turtle_colors)
+turtles.draw()
 
 #	
-  startx = startx + 50
-  starty = starty + 50
+# startx = startx + 50
+# starty = starty + 50
 
 wn = trtl.Screen()
 wn.mainloop()
