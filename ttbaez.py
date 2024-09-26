178d83835810d9a4cc0d08ecc9d29f8c970e8476
BaeztheProg <baezlopezolsen@dublinusd.org>
2024-09-26 13:25:08 -0700

    finished step 13 and 14. Step thirteen is implemented on the Draw on last method "drawOnLast()" The init contains the step 14 with different colors for each turtle using the pop method. Randomization was used to add some flavor.

--- ttbaez.py
+++ ttbaez.py
@@ -1,6 +1,7 @@
 #   a117_traversing_turtles.py
 #   Add code to make turtles move in a circle and change colors.
 import turtle as trtl
+import random as rand
 
 
 class muchasTortugas:
@@ -12,7 +13,8 @@ class muchasTortugas:
     self.turtles = []
     for each in range(self.numTurtles):
       t = trtl.Turtle(shape=self.shapes[each])
-      t.color(colors[each])
+      #random color per color list
+      t.color(colors.pop(rand.randint(0, len(colors)-1)))
       self.turtles.append(t)
   def draw(self):
     startx = 0
@@ -25,7 +27,19 @@ class muchasTortugas:
       t.forward(50)
       startx = startx + 50
       starty = starty + 50
-    
+   
+  def drawOnLast(self):
+    startx = 0
+    starty = 0
+    for t in self.turtles:
+      t.penup()
+      t.goto(startx, starty)
+      t.pendown()
+      t.right(45)     
+      t.forward(50)
+      startx = t.xcor()
+      starty = t.ycor()
+
 
 
 
@@ -39,7 +53,7 @@ turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
 turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 
 turtles = muchasTortugas(6, turtle_shapes, turtle_colors)
-turtles.draw()
+turtles.drawOnLast()
 
 #	
 # startx = startx + 50
